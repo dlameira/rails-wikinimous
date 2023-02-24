b0dac6e0c05bfc445de05ae96ef34ab3e944547b
Daniel Lameira <dlameira@gmail.com>
2023-02-24 07:43:04 -0300

    stylizing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+10.times do
+  post = Article.new
+  post.title = Faker::Movies::Lebowski.quote
+  post.content = Faker::Books::Dune.quote
+  post.save
+end
